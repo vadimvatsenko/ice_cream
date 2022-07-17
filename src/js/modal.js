6dc62add07b16d0134a87a99d63b7d6760388002
Vadim Vatsenko <vadim.vatsenko@gmail.com>
2022-07-17 20:56:14 +0300

    fix modal

--- src/js/modal.js
+++ src/js/modal.js
@@ -16,3 +16,21 @@
     refs.body.classList.toggle('no-scroll');
   }
 })();
+
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('[modal-open-mobile]'),
+    closeModalBtn: document.querySelector('[modal-close-mobile]'),
+    modal: document.querySelector('[header-modal]'),
+    body: document.querySelector('body'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+    refs.body.classList.toggle('no-scroll');
+  }
+})();
+
