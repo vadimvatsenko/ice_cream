70ac1211b0224b6a29f9082a98b23b24d14f9f82
vadimvatsenko <69381493+vadimvatsenko@users.noreply.github.com>
2022-07-18 14:01:52 +0000

    Deploying to gh-pages from @ vadimvatsenko/ice_cream@032412a190f345f5b64e8da8507e6e43d9f66fb9 🚀

---
+++ index.21a721ed.js
@@ -0,0 +1,2 @@
+!function(){var o={openModalBtn:document.querySelector("[data-map-modal-open]"),closeModalBtn:document.querySelector("[data-map-modal-close]"),modal:document.querySelector("[data-map-modal]"),body:document.querySelector("body")};function e(){o.modal.classList.toggle("is-hidden"),o.body.classList.toggle("no-scroll")}o.openModalBtn.addEventListener("click",e),o.closeModalBtn.addEventListener("click",e)}();
+//# sourceMappingURL=index.21a721ed.js.map
